c736508c50409428c46efab7efea26e15294c1d6
KoEonYack <21300035@handong.edu>
2020-05-30 20:43:18 +0900

    promise 적용

--- controllers/subscribersController.js
+++ controllers/subscribersController.js
@@ -1,10 +1,38 @@
 const Subscriber = require("../models/subscriber");
 
 exports.getAllSubscribers = (req, res, next) => {
-    Subscriber.find({}, (err, subscirbers) => {
-        if (err) next(err);
-        req.data = subscirbers;
-        next();
+    Subscriber.find({})
+    .exec()
+    .then((subscribers) => {
+        res.render("subscribers", {
+            subscribers: subscribers
+        });
+    })
+    .catch((error) => {
+        console.log(error.message);
+        return [];
+    })
+    .then(() => {
+        console.log("promise complate");
     });
 };
 
+exports.getSubscriptionPage = (req, res) => {
+    res.render("contact");
+};
+
+exports.saveSubscriber = (req, res) => {
+    let newSubscriber = new Subscriber({
+      name: req.body.name,
+      email: req.body.email,
+      zipCode: req.body.zipCode
+    });
+    newSubscriber
+      .save()
+      .then(result => {
+        res.render("thanks");
+      })
+      .catch(error => {
+        if (error) res.send(error);
+      });
+  };
--- index.js
+++ index.js
@@ -48,7 +48,8 @@ app.get("/subscribers", subscribersController.getAllSubscribers, (req, res, next
     res.render("subscribers", { subscribers: req.data });
 })
 
-
+app.get("/contact", subscribersController.getSubscriptionPage);
+app.post("/subscribe", subscribersController.saveSubscriber);
 
 
 app.listen(PORT, () => {
