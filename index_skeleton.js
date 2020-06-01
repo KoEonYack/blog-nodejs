8e9bb6b7b817ff209c9c6d6c254c4c655cb5d123
KoEonYack <21300035@handong.edu>
2020-06-01 13:50:38 +0900

    add index skeleton code

---
+++ index_skeleton.js
@@ -0,0 +1,46 @@
+
+
+
+const express = require('express');
+const app = express();
+const homeController = require("./controllers/homeController");
+const errorController = require("./controllers/errorController");
+const layouts = require("express-ejs-layouts");
+
+app.set("view engine", "ejs");
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+)
+
+app.use(express.json());
+app.use(layouts);
+app.use(express.static("public"));  
+
+
+const PORT = 5003;
+
+app.get('/hello', (req, res) => 
+    res.send("Hello World!")
+);
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+
+app.get("/courses", homeController.showCourses);
+app.get("/contact", homeController.showSignUp);
+app.post("/contact", homeController.postedSignUpForm);
+
+
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+})
+
